72ff447a3196f0de4627f86cd8f7d7c5b8b8f7b0
Sam Bristol <samuel.d.bristol@gmail.com>
2015-10-20 02:36:01 +0000

    Add test for title to be a minimum of 1 character

--- flashcardapp/test/models/flashcard_test.rb
+++ flashcardapp/test/models/flashcard_test.rb
@@ -5,4 +5,10 @@ class FlashcardTest < ActiveSupport::TestCase
     flashcard = Flashcard.new
     assert flashcard.save
   end
+  
+  test "title should be at least 1 character" do
+    flashcard = Flashcard.new
+    flashcard.title = ""
+    assert !flashcard.save
+  end
 end
