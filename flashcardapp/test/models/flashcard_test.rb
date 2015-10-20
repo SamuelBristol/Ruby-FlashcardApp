2be200b0bd2aa717f095fbccdd3e0be3d4cb38c5
Sam Bristol <samuel.d.bristol@gmail.com>
2015-10-20 02:32:55 +0000

    Add test to create a valid record

--- flashcardapp/test/models/flashcard_test.rb
+++ flashcardapp/test/models/flashcard_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class FlashcardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a valid record" do
+    flashcard = Flashcard.new
+    assert flashcard.save
+  end
 end
