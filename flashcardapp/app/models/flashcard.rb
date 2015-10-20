19dffc1271f61a6b72a6089b884902e6124656e5
Sam Bristol <samuel.d.bristol@gmail.com>
2015-10-20 03:11:59 +0000

    Require presence of answer, add test to confirm

--- flashcardapp/app/models/flashcard.rb
+++ flashcardapp/app/models/flashcard.rb
@@ -1,3 +1,3 @@
 class Flashcard < ActiveRecord::Base
-    validates_presence_of :title
+    validates_presence_of :title, :answer
 end
--- flashcardapp/test/models/flashcard_test.rb
+++ flashcardapp/test/models/flashcard_test.rb
@@ -4,12 +4,14 @@ class FlashcardTest < ActiveSupport::TestCase
   test "should create a valid record" do
     flashcard = Flashcard.new
     flashcard.title = "flashcard"
+    flashcard.answer = "a tool used to study"
     assert flashcard.save
   end
   
   test "title should be at least 1 character" do
     flashcard = Flashcard.new
     flashcard.title = ""
+    flashcard.answer = "empty title"
     assert !flashcard.save
     flashcard.title = "a"
     assert flashcard.save
@@ -20,5 +22,7 @@ class FlashcardTest < ActiveSupport::TestCase
     flashcard.title = "a"
     flashcard.answer = ""
     assert !flashcard.save
+    flashcard.answer = "the first letter of the english alphabet"
+    assert flashcard.save
   end
 end
